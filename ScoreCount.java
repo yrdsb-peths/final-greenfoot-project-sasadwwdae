85bec2672fc4d2e5778aa9064606415f07be02bd
asdafwr <105795748+sasadwwdae@users.noreply.github.com>
2022-12-20 13:11:49 -0500

    try to create a score screen and able to return the value

---
+++ ScoreCount.java
@@ -0,0 +1,34 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Score here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ScoreCount extends Actor
+{
+    private Player pc;
+    GreenfootImage score;
+    // Handles score count display on top right of all worlds
+    public ScoreCount(Player pc)
+    {
+        this.pc = pc;
+        score = new GreenfootImage(144, 50);
+        setImage(score);
+        Font font = new Font("Agency FB", 30);
+        score.setFont(font);
+        score.setColor(Color.WHITE);
+    }
+    public void updateScore()
+    {
+        score.clear(); // used to prevent overlapping label
+        score.drawString(Integer.toString(pc.score), 72, 25); // draw onto screen
+        setImage(score);
+    }
+    public void act()
+    {
+        updateScore(); // if the score increases, this will update it
+    }
+}
+
